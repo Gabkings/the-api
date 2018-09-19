2811324a250ef4ef7fcc8ddb85c9df210f70727a
unknown <gabworks51@gmail.com>
2018-09-19 11:16:55 +0300

    This feature implements the adding order operation. the user is able to place a new order this order is automatically added to the oders list [finishes #160611942]

---
+++ app/api/v1/orders.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from flask_restful import Resource
+from app.model import Order, orders
+
+class DisplayOrders(Resource):
+    def post(self):
+        data = request.get_json()
+        order = Order(data['name'], data["price"],data['description'])
+        orders.append(order)
+
+        return {"message":"Food order has been created"}, 201
