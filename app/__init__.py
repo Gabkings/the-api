bb68fe3ae84706b2d72188517bb088bfb6b05927
unknown <gabworks51@gmail.com>
2018-10-02 02:34:06 +0300

    [feature #160841056] creating new user account [finishes #160841056]

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from instance.config import app_config
 from .api.v1.views.orders import OrderOpreations,DisplayOrders
-from .api.v2.users.users import Users,Login
+from .api.v2.users.users import Users
 from .api.v2.models.db import init_db
 
 
@@ -11,10 +11,8 @@ def create_app(config_stage):
     Gabriel.config.from_object(app_config[config_stage])
     api = Api(Gabriel)
     init_db()
-    api.add_resource(OrderOpreations, '/api/v1/orders/<int:id>')
-    api.add_resource(DisplayOrders, '/api/v1/orders')
-    api.add_resource(Users, '/api/v2/users')
-    api.add_resource(Login,'/api/v2/login')
+    api.add_resource(Users, '/api/v2/auth/signup')
+
 
 
     return Gabriel
--- app/api/v2/models/db.py
+++ app/api/v2/models/db.py
@@ -9,7 +9,6 @@ from .tables import queries
 
 def db():
     url = os.environ.get('DATABASE_URL')
-    print(url)
     conn = psycopg2.connect(url)
     # print(db_url)
     # conn = psycopg2.connect(db_url)
@@ -27,12 +26,9 @@ def init_db():
 
         # activate connection cursor
         cur = connection.cursor()
-        res = cur.execute("SELECT * FROM users")
-        print(res)
         for query in queries:
             cur.execute(query)
         connection.commit()
-        print("database connected")
     except (Exception, psycopg2.DatabaseError) as error:
         print("Database not connected")
         print(error)
