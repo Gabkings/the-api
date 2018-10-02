fa1f75ea46bee8adae6dc60743cad90483d4cac5
unknown <gabworks51@gmail.com>
2018-10-02 10:22:43 +0300

    [feature #160841091] get a specific order details by id

--- app/api/v2/orders/orders.py
+++ app/api/v2/orders/orders.py
@@ -2,9 +2,11 @@ from flask import Flask, request,jsonify
 from flask_restful import Resource,reqparse
 import psycopg2
 from ..models.db import db
+from ..users.auth import check_auth
 
 class Orders(Resource):
-    def get(self):
+    @check_auth
+    def get(current_user,self):
         """get all orders"""
         try:
             conn = db()
@@ -15,6 +17,25 @@ class Orders(Resource):
                 return {"message":"No orders available"}
             else:
                 return {"Orders": orders}
+        except (Exception, psycopg2.DatabaseError) as error:
+            cur.execute("rollback;")
+            print(error)
+            return {'Message': 'current transaction is aborted'}, 500
+
+class Specific_order_detail(Resource):
+    @check_auth
+    def get(current_user,self,order_id):
+        try:
+            conn = db()
+            cur = conn.cursor()
+            cur.execute("SELECT * from orders WHERE order_id = order_id")
+            orders = cur.fetchone()
+            order_list = []
+            order_list.append(orders)
+            if len(order_list) < 1:
+                return {"message":"No orders available"}
+            else:
+                return jsonify({"Orders": order_list})
         except (Exception, psycopg2.DatabaseError) as error:
             cur.execute("rollback;")
             print(error)
