ee14fd7c824d34374773dba30ec83767d20fe4ca
unknown <gabworks51@gmail.com>
2018-10-02 09:40:07 +0300

    [feature #160841073] get all the orders that exists in the database

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_restful import Api
 from instance.config import app_config
 from .api.v1.views.orders import OrderOpreations,DisplayOrders
 from .api.v2.users.users import Users,Login
-from .api.v2.meals.meals import Food
+from .api.v2.meals.meals import Meals
+from .api.v2.orders.orders import Orders
 from .api.v2.models.db import init_db
 
 
@@ -14,7 +15,8 @@ def create_app(config_stage):
     init_db()
     api.add_resource(Users, '/api/v2/auth/signup')
     api.add_resource(Login, '/api/v2/auth/login')
-    api.add_resource(Food, '/api/v2/menu')
+    api.add_resource(Meals, '/api/v2/menu')
+    api.add_resource(Orders, '/api/v2/orders')
 
 
 
