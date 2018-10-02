dbfe1da1a940440157f5ed16f6f248234a5326af
unknown <gabworks51@gmail.com>
2018-10-02 04:16:25 +0300

    [feature #160841108] this impements the adding of a new food to the existing menu

--- app/api/v2/models/tables.py
+++ app/api/v2/models/tables.py
@@ -9,7 +9,7 @@ users = """CREATE TABLE if not exists users (
 meals_table = """
 CREATE TABLE IF NOT EXISTS meals (
     meal_id serial PRIMARY KEY,
-    name VARCHAR(25) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     price INT NOT NULL,
     description VARCHAR(25) NOT NULL
 )
