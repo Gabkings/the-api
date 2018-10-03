c630f98cda1610f57be912c570316ffea3558e8d
unknown <gabworks51@gmail.com>
2018-10-03 04:37:01 +0300

    [feature #160841065] and a unique order for the user users

--- app/api/v2/orders/users_orders.py
+++ app/api/v2/orders/users_orders.py
@@ -21,3 +21,58 @@ class User_orders(Resource):
             cur.execute("rollback;")
             print(error)
             return {'Message': 'current transaction is aborted'}, 500
+
+    @check_auth
+    def post(user,self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+
+        parser.add_argument(
+            'user_id',
+            type=int,
+            required=True,
+            help="user id is required"
+        )
+        parser.add_argument(
+            'name',
+            type=str,
+            required=True,
+            help="name is required"
+        )
+        parser.add_argument(
+            'price',
+            type=float,
+            required=True,
+            help="passwprd is required"
+        )
+        parser.add_argument(
+            'address',
+            type=str,
+            required=True,
+            help="address is required"
+        )
+
+        data = parser.parse_args()
+        user_id = data["user_id"]
+        name = data["name"]
+        price = data["price"]
+        address = data["address"]
+
+        try:
+            conn = db()
+            cur = conn.cursor()
+
+            cur.execute("SELECT * FROM orders WHERE name = %(name)s",
+                        {'name': data['name']})
+
+            # check if order exist
+            if cur.fetchone() is not None:
+                return {'Message': 'order already exist'}
+            cur.execute("INSERT INTO orders (user_id, name, price, address) VALUES (%(user_id)s, %(name)s, %(price)s, %(address)s);", {
+                'user_id': data["user_id"], 'name': data["name"], 'price': data["price"],'address':data['address']})
+            conn.commit()
+            return {'Message': 'Order created successfully'}, 201
+        except (Exception, psycopg2.DatabaseError) as error:
+            cur.execute("rollback;")
+            print(error)
+            return {'Message': 'current transaction is aborted'}, 500
+
